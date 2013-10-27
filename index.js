cf2f2ee025e11307a582add709e28f2e0a35acef
Ryan Ramage <ryan.ramage@gmail.com>
2013-10-27 11:57:40 -0600

    add real keyboard

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 var express = require('express'),
+    hid = require('hidstream'),
+    devices = hid.getDevices(),
     FakeKeyboard = require('./test/keyboard_mock'),
     Keyboard = require('./lib/keyboard'),
     Game = require('./lib/game'),
@@ -8,9 +10,11 @@ var express = require('express'),
 
 server.listen(8081);
 
+console.log(devices);
 
-var fake = new FakeKeyboard();
-var keyboard = new Keyboard(fake);
+var actual_keyboard = new hid.device(devices[0].path);
+//var fake = new FakeKeyboard();
+var keyboard = new Keyboard(actual_keyboard);
 var game = new Game(keyboard);
 
 
