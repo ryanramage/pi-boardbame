6ec7a82033a2f1479ee4656057960e19f3ffa8c5
Ryan Ramage <ryan.ramage@gmail.com>
2013-10-27 08:21:22 -0600

    make hostname portable

--- static/js/app.js
+++ static/js/app.js
@@ -1,4 +1,7 @@
-var socket = io.connect('http://localhost');
+
+var connection = window.location.hostname;
+var socket = io.connect('http://' + connection);
+
 socket.on('keyboard', function (data) {
     console.log(data);
 
